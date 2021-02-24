f9fd9d279bec937f43a713078803ebf989afcd60
dancatlinh <77012055+dancatlinh@users.noreply.github.com>
2021-02-24 20:44:49 +0700

    Add files via upload

--- Abstract Class Interface/Baitap/src/ColorableTest.java
+++ Abstract Class Interface/Baitap/src/ColorableTest.java
@@ -1,12 +1,12 @@
 public class ColorableTest {
     public static void main(String[] args) {
-        Square square = new Square();
-        Circle circle = new Circle();
-        Shape[] shape = { new Circle(), new Square()};
+        Shape[] shape = { new Circle(3.5, "red", false), new Square()};
 
         for (int j = 0; j<shape.length; j++) {
             if (shape[j] instanceof Colorable) {
-                square.howToColor();
+                ((Colorable) shape[j]).howToColor();
+            } else {
+                System.out.println(((Circle) shape[j]).getArea());
             }
         }
     }
